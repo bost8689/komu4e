2efea635f52a3781682d60b2a72f4d80e686e9a5
Bogdanov Stanislav <bost8689@yandex.ru>
2019-06-25 15:51:49 +0500

    save

---
+++ local/resources/views/komuche_ndm/order/processing_add_order.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@extends('komuche_ndm.layouts.sidebar')
+
+@section('main')        
+          <div class="col-sm-8"> 
+            <div class="card">
+            <div class="card-header"> Добавление заказа           
+            </div>
+            <div class="card-body">
+            
+            </div>
+            </div>
+          </div>         
+@endsection('main')
